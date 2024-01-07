35fe534e6590d21ce4b0d8b3c2ee7cff6c360966
sweetenpotato <69999133+go9um4@users.noreply.github.com>
2024-01-08 00:34:21 +0900

    init: Project

---
+++ src/main/java/io/github/vitelotte/advancementbenefit/AdvancementBenefit.java
@@ -0,0 +1,17 @@
+package io.github.vitelotte.advancementbenefit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancementBenefit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
