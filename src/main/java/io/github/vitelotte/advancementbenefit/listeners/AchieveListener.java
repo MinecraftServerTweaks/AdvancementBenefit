80de342374a2566654f87fc589e122831fe13bbe
sweetenpotato <69999133+go9um4@users.noreply.github.com>
2024-01-08 00:51:25 +0900

    feat: increase max health when player achieved new advancement

--- src/main/java/io/github/vitelotte/advancementbenefit/listeners/AchieveListener.java
+++ src/main/java/io/github/vitelotte/advancementbenefit/listeners/AchieveListener.java
@@ -1,6 +1,19 @@
 package io.github.vitelotte.advancementbenefit.listeners;
 
+import org.bukkit.attribute.Attributable;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 
 public class AchieveListener implements Listener {
+
+    @EventHandler
+    public void onAchieve(PlayerAdvancementDoneEvent event) {
+        Player player = event.getPlayer();
+        AttributeInstance maxHealthAttribute = player.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        maxHealthAttribute.setBaseValue(maxHealthAttribute.getBaseValue() + 1);
+    }
 }
